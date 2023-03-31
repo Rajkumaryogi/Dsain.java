024bc604848365e6949fd02937bfbf43e56819e8
Rajkumaryogi <99001136+Rajkumaryogi@users.noreply.github.com>
2023-03-31 09:50:35 +0530

    Update ifelsein.java

--- ifelsein.java
+++ ifelsein.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class ifelsein {
     public static void main(String[] args) {
-        //Conditional Statements
+        //Conditional Statements - If else
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter age");
         int age = sc.nextInt();
