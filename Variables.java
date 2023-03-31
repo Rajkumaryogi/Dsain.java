b7cf0c279a09cb98f538bc66f7b5d3e0d7a89236
Rajkumaryogi <99001136+Rajkumaryogi@users.noreply.github.com>
2023-03-31 09:45:06 +0530

    This for Basics of java
    Concepts are in theory form and Questions form

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Variables in java
+        int a = 20;
+        String name = "Rajkumar Yogi";
+        double price = 29.34;
+
+        a = 38;
+        name = "Yogi";
+        System.out.println(name);
+
+    }
+    
+}
